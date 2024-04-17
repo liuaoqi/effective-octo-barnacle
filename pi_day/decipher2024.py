f228c4eb311acd9c96344938156361712c17296c
aoqi.liu@mail.utoronto.ca <aoqi.liu@mail.utoronto.ca>
2024-04-17 15:21:15 -0400

    Pi 2024

--- pi_day/decipher2024.py
+++ pi_day/decipher2024.py
@@ -1,24 +1,6 @@
-# Welcome, Agent!
-# You've intercepted a secret message encoded using a shift cipher related to the digits of pi.
-# Your mission, should you choose to accept it, is to decode this message and unveil the truth hidden within.
-
-# Part 1: Decipher the Message
-# Implement an algorithm that decipher a message using the pi key. The key consist on the first 16 numbers of
-# the constant Pi. Each letter of the message is shifted by the digit of the key placed in the same position.
-# Blank spaces and non-letter characters are not shifted, but still part of the message. After the first 16 
-# positions in the message, the shift restarts with the first digit of the key.
-
-# Enconded Message:
-# Wii kxtszof ova fsegyrpm d lnsrjkujvq roj! Kdaxii svw vnwhj pvugho buynkx tn vwh-gsvw ruzqia. Mrq'x kxtmjw bx fhlhlujw cjoq! Hmg tyhfa gx dwd fdqu bsm osynbn oulfrex, kahs con vjpmd qtjv bx whwxssp cti hmulkudui f Jgusd Yp Gdz!
-
-# Part 2: Find the Secret Numerical Code
-# Once you've deciphered the message, your next task is to find a hidden secret numerical code. Within the message,
-# between the words (even between separated words by blank spaces, commas or dots), a series of numbers (from one to ten)
-# written in text are concealed. The challenge requires you to calculate the multiplication of all the digits contained in the message.
-
-# Example
-# In the text We need to waterprooF OUR boots to meet the virtuoSI Xylophonists are hidden the numbers 4 and 6. Multiplying both you 4 * 6 = 24.
-
+'''
+    https://ivanr3d.com/projects/pi/
+'''
 import math
 
 secret = "Wii kxtszof ova fsegyrpm d lnsrjkujvq roj! Kdaxii svw vnwhj pvugho buynkx tn vwh-gsvw ruzqia. Mrq'x kxtmjw bx fhlhlujw cjoq! Hmg tyhfa gx dwd fdqu bsm osynbn oulfrex, kahs con vjpmd qtjv bx whwxssp cti hmulkudui f Jgusd Yp Gdz!"
